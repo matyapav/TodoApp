faa0b5d2e1d35adb2f98ce547e90acc8cee626ab
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-04-28 20:10:11 +0200

    Initial commit

---
+++ app/src/main/java/cz/matyapav/todoapp/TodoDayFragment.java
@@ -0,0 +1,20 @@
+package cz.matyapav.todoapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Pavel Matyáš (matyapav@fel.cvut.cz).
+ * @since 1.0.0..
+ */
+public class TodoDayFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.)
+    }
+}
